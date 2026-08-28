7298119ad6d0e9adff39e6195d1582befc907ce2
Tran Mai Cong Hung <tranmaiconghung@gmail.com>
2026-08-28 12:03:01 +0700

    implement sign in and sign up with validation logic/input constraints

---
+++ src/main/java/com/se196693/mvc/service/UserService.java
@@ -0,0 +1,11 @@
+package com.se196693.mvc.service;
+
+import com.se196693.mvc.entity.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    User findByUsername(String username);
+
+    User createUser(User user);
+}
