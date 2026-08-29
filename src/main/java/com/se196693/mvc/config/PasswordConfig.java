063a850d1f51a8f4d456b45d3978c495d4e1de9c
Tran Mai Cong Hung <tranmaiconghung@gmail.com>
2026-08-29 15:10:58 +0700

    fix conflix

--- src/main/java/com/se196693/mvc/config/PasswordConfig.java
+++ src/main/java/com/se196693/mvc/config/PasswordConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@Configuration// đánh dấu class là một cấu hình, cho phép Spring quản lý các bean được định nghĩa trong class này
 public class PasswordConfig {
 
-    @Bean
+    @Bean //lưu các đối tượng được khai báo là Bean vào trong Spring container
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); //tạo ra một object BCryptPasswordEncoder, 
     }
 }
