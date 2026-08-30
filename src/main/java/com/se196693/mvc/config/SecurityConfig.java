3c63bb909a02b773389abc953f831548e0914dd0
Tran Mai Cong Hung <tranmaiconghung@gmail.com>
2026-08-30 14:41:44 +0700

    fix convert object

--- src/main/java/com/se196693/mvc/config/SecurityConfig.java
+++ src/main/java/com/se196693/mvc/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.se196693.mvc.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity// bật method-level authorization
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -34,7 +36,6 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/error"
                         ).permitAll()
-                                .requestMatchers("/api/users/**").hasRole("ADMIN")
 //                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
--- src/main/java/com/se196693/mvc/controller/UserController.java
+++ src/main/java/com/se196693/mvc/controller/UserController.java
@@ -7,6 +7,7 @@ import com.se196693.mvc.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PreAuthorize("hasRole('ADMIN')")//SC, phân quyền trước khi method dc thực thi, cần khai báo @EnableMehthodSecutiry
     @PostMapping
     public ResponseEntity<UserResponse> createUser(
            @Valid @RequestBody UserCreationRequest request){
